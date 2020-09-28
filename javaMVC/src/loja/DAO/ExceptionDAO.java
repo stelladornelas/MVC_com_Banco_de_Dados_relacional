ba005ae6977933fd97b3b8db4896705d522f6f10
stelladornelas <63124180+stelladornelas@users.noreply.github.com>
2020-09-28 10:35:41 -0300

    Create ExceptionDAO.java

---
+++ javaMVC/src/loja/DAO/ExceptionDAO.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package loja.DAO;
+
+/**
+ *
+ * @author Usuário
+ */
+public class ExceptionDAO extends Exception{
+     public ExceptionDAO(String mensagem){
+        
+                super(mensagem);
+                
+                
+     }
+    
+}
